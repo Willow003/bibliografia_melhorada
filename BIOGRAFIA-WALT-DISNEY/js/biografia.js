33c0ccea7209ec63776fb46c6be4347e8f82fb03
Willow003 <144964024+Willow003@users.noreply.github.com>
2023-11-30 11:19:51 -0300

    bibliografia melhorada do Walt Disney

---
+++ BIOGRAFIA-WALT-DISNEY/js/biografia.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+function nextSlide() {
+    const slides = document.querySelectorAll('.slide');
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+setInterval(nextSlide, 3000);
